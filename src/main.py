1b6d5f213b41f892c732e897f235c00ef03cfa28
ppulidodev <pablopulido.dev@gmail.com>
2025-05-25 13:33:52 +0200

    fix the css loading

--- src/main.py
+++ src/main.py
@@ -16,7 +16,7 @@ def main():
         os.system(f"rm -rf {dest_dir}")
 
     print("Copying static files to docs directory...")
-    os.system(f"cp -r static {dest_dir}")
+    os.system(f"cp -r static/* {dest_dir}/")
 
     print("Generating content...")
     generate_pages_recursive(content_dir, template_path, dest_dir, basepath)
